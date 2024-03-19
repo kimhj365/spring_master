639e09f16be0db6e9124fcb449a167b1463df3db
kimhj365 <khj6428@gmail.com>
2024-03-20 02:46:51 +0900

    20240320 Spring

--- board/src/main/java/com/example/demo/board/web/BoardController.java
+++ board/src/main/java/com/example/demo/board/web/BoardController.java
@@ -10,21 +10,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.demo.board.BoardVO;
+import com.example.demo.board.SearchVO;
 import com.example.demo.board.mapper.BoardMapper;
 
 @Controller
 public class BoardController {
 	@Autowired BoardMapper dao;
 	
+	// ajax 목록 조회
 	@RequestMapping("/ajaxBoard")
 	@ResponseBody
-	public List<BoardVO> ajaxBoard() {
-		return dao.getBoardList(null, null);
+	public List<BoardVO> ajaxBoard(BoardVO vo, SearchVO svo) {
+		return dao.getBoardList(vo, svo);
 	}
 	
-	@RequestMapping("/boardList")
-	public String boardList(Model model) {
-		model.addAttribute("boardList", dao.getBoardList(null, null));
-		return "boardList";
+	// 목록 조회
+	@RequestMapping("/board/boardList")
+	public String boardList(Model model, BoardVO vo, SearchVO svo) {
+		model.addAttribute("boardList", dao.getBoardList(vo, svo));
+		return "board/boardList";
 	}
 }
